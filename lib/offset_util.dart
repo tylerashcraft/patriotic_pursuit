7b75ecc94cc40fcc7bf10d3cf8032fd23b2006cb
Tyler Ashcraft <d.tyler.ashcraft@gmail.com>
2025-06-01 22:45:05 -0400

    Web release

--- lib/offset_util.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-
-class OffsetUtil {
-  static Offset getScaledOffset(Offset offset, Size size) {
-    return Offset(offset.dx * size.width / 1728 + size.width / 2, offset.dy * size.height / 936 + size.height / 2);
-  }
-}
